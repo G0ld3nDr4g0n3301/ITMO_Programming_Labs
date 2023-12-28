f06bc3c1c60dc91482f1dcef9450c33eeedc5ba0
G0ld3nDr4g0n3301 <ainsoph_itmo@mail.ru>
2023-12-28 22:41:06 +0300

    Corrected a build script.Corrected UML diagram.

--- lab4/scripts/build.sh
+++ lab4/scripts/build.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 
 cd src
-javac -d ../bin -cp .:interfaces/:beds:misc:watering:shorties Main.java */*.java
+javac -d ../bin -cp .:interfaces/:beds:misc:watering:shorties:exceptions Main.java */*.java
 cd ../bin/
 jar -cfm ../lab4.jar ../META-INF/manifest.mf Main.class */*.class
 
